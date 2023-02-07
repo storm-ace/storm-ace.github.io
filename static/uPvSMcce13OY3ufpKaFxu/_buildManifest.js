79db7131a0856f27c2603a1380f412cdf711097a
Stormace <stormacework@gmail.com>
2023-02-07 17:02:09 +0100

    Build

---
+++ static/uPvSMcce13OY3ufpKaFxu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d0447323-f16a634d945b80f4.js","static/chunks/0c428ae2-fe29ae5490022300.js","static/chunks/pages/index-1b148d44f7ef019b.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
